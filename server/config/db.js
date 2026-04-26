f9d46f54a4b6cdd6e36640bde102bdb48985d01e
edwalv1981-design <edwalv1981@gmail.com>
2026-04-26 18:05:06 -0500

    Fix técnico: Soporte para DATABASE_URL y SSL en PostgreSQL

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,36 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-    logging: false,
-    define: {
-        timestamps: true,
-        underscored: true,
-    }
-});
+let sequelize;
+
+// LÓGICA HÍBRIDA: Railway (URL) o Local (Variables separadas)
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // Vital para Railway/Render/AWS
+            }
+        },
+        define: {
+            timestamps: true,
+            underscored: true,
+        }
+    });
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        logging: false,
+        define: {
+            timestamps: true,
+            underscored: true,
+        }
+    });
+}
 
 const connectDB = async () => {
     try {
